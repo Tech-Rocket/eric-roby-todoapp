0235b437a3d7a904f785f869237cad2bcb413a62
Ebee02 <beelawrence8@gmail.com>
2025-03-12 12:37:20 +0000

    Initial commit

---
+++ database.py
@@ -0,0 +1,24 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+# the url is just the location of the db
+SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+
+
+# after creating the url, you need to import
+# create_engine from sqlalchemy, and write a variable
+# and assign the SQLALCHEMY_DATABASE_URL to it
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+
+# next you create the SessionLocal by importing
+# the sessionmaker from sqlalchemy.orm
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# finally you need to create an object of the database
+# that you can import in another file
+Base = declarative_base()
---
+++ main.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI, Depends, HTTPException, Path, Query
+import models
+from models import Todos
+from database import engine, SessionLocal
+from typing import Annotated
+from sqlalchemy.orm import Session
+from starlette import status
+
+
+app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+@app.get("/", status_code=status.HTTP_200_OK)
+async def read_all(db: db_dependency):
+    return db.query(Todos).all()
+
+
+@app.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
+async def fetch_todo_by_id(db: db_dependency, todo_id: int = Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is not None:
+        return todo_model
+    raise HTTPException(status_code=404, detail="Todo not found")
+
+
+# run .mode table to change the look of the tables in sqlite
