f6c75b79c98741c8fa38ce7b60e90a346712a6a5
Ebee02 <beelawrence8@gmail.com>
2025-03-12 13:59:32 +0000

    Added the post method on the create_todo func within the main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Depends, HTTPException, Path, Query
+from pydantic import BaseModel, Field
 import models
 from models import Todos
 from database import engine, SessionLocal
@@ -23,6 +24,13 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
+class TodoRequest(BaseModel):
+    title: str = Field(min_length=3)
+    description: str = Field(min_length=3, max_length=100)
+    priorty: int = Field(gt=0, lt=6)
+    complete: bool
+
+
 @app.get("/", status_code=status.HTTP_200_OK)
 async def read_all(db: db_dependency):
     return db.query(Todos).all()
@@ -34,6 +42,13 @@ async def fetch_todo_by_id(db: db_dependency, todo_id: int = Path(gt=0)):
     if todo_model is not None:
         return todo_model
     raise HTTPException(status_code=404, detail="Todo not found")
+
+
+@app.post("/todo", status_code=status.HTTP_201_CREATED)
+async def create_todo(db: db_dependency, todo_request: TodoRequest):
+    todo_model = Todos(**todo_request.model_dump())
+    db.add(todo_model)
+    db.commit()
 
 
 # run .mode table to change the look of the tables in sqlite
