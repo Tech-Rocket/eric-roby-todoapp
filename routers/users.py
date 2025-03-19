9b6e06d5796634b49cbb92ed528376f8654ff2b0
Ebee02 <beelawrence8@gmail.com>
2025-03-19 15:57:43 +0000

    Created users route and added it to the main.py file

---
+++ routers/users.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter, Depends, HTTPException, Path
+from pydantic import BaseModel, Field
+from models import Todos
+from database import SessionLocal
+from typing import Annotated
+from sqlalchemy.orm import Session
+from starlette import status
+from .auth import get_current_user
+
+
+router = APIRouter(prefix="/", tags=["users"])
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
+
+
+@router.app("/", status_code=status.HTTP_200_OK)
+async def read_all():
+    return {"message": "user data"}
