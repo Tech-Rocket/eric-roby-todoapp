72b58f13f155af8196e2b10f67cbee9d88b515c4
Ebee02 <beelawrence8@gmail.com>
2025-03-19 16:10:57 +0000

    update

--- routers/users.py
+++ routers/users.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException, Path
 from pydantic import BaseModel, Field
-from models import Todos
+from models import Users
 from database import SessionLocal
 from typing import Annotated
 from sqlalchemy.orm import Session
@@ -24,5 +24,8 @@ user_dependency = Annotated[dict, Depends(get_current_user)]
 
 
 @router.get("/", status_code=status.HTTP_200_OK)
-async def read_all():
-    return {"message": "user data"}
+async def get_user(user: user_dependency, db: db_dependency):
+    if user is None:
+        raise HTTPException(status_code=401, detail="Authenication Failed")
+
+    return db.query(Users).all()
