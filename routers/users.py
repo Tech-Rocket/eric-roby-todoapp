3104e9c10f11a6f7b14ac72301662b949c0a240e
Ebee02 <beelawrence8@gmail.com>
2025-03-20 08:06:47 +0000

    update

--- routers/users.py
+++ routers/users.py
@@ -26,7 +26,8 @@ bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 class ChangePasswordRequest(BaseModel):
-    hashed_password: str = Field(min_length=3, max_length=50)
+    password: str
+    new_password: str = Field(min_length=3, max_length=50)
 
 
 @router.get("/", status_code=status.HTTP_200_OK)
@@ -37,19 +38,23 @@ async def get_user(user: user_dependency, db: db_dependency):
     return db.query(Users).filter(Users.id == user.get("id")).first()
 
 
-@router.put("/change_password/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
+@router.put("/password", status_code=status.HTTP_204_NO_CONTENT)
 async def change_password(
     user: user_dependency,
     db: db_dependency,
     change_password_request: ChangePasswordRequest,
-    user_id: int = Path(gt=0),
 ):
     if user is None:
         raise HTTPException(status_code=401, detail="Authenication Failed")
-    user_model = db.query(Users).filter(Users.id == user_id).first()
+    user_model = db.query(Users).filter(Users.id == user.get("id")).first()
 
-    if user_model is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    user_model.hashed_password = change_password_request.hashed_password
+    if not bcrypt_context.verify(
+        change_password_request.password, user_model.hashed_password
+    ):
+        raise HTTPException(status_code=404, detail="Error on password change")
+
+    user_model.hashed_password = bcrypt_context.hash(
+        change_password_request.new_password
+    )
     db.add(user_model)
     db.commit()
