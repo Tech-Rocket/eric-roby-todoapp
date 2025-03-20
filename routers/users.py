755fb0571c7a305a81a56fabab6818da20033c0c
Ebee02 <beelawrence8@gmail.com>
2025-03-20 07:34:21 +0000

    update

--- routers/users.py
+++ routers/users.py
@@ -6,9 +6,10 @@ from typing import Annotated
 from sqlalchemy.orm import Session
 from starlette import status
 from .auth import get_current_user
+from passlib.context import CryptContext
 
 
-router = APIRouter(prefix="/user", tags=["users"])
+router = APIRouter(prefix="/user", tags=["user"])
 
 
 def get_db():
@@ -21,6 +22,11 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
 user_dependency = Annotated[dict, Depends(get_current_user)]
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+class ChangePasswordRequest(BaseModel):
+    hashed_password: str = Field(min_length=3, max_length=50)
 
 
 @router.get("/", status_code=status.HTTP_200_OK)
@@ -28,4 +34,22 @@ async def get_user(user: user_dependency, db: db_dependency):
     if user is None:
         raise HTTPException(status_code=401, detail="Authenication Failed")
 
-    return db.query(Users).all()
+    return db.query(Users).filter(Users.id == user.get("id")).first()
+
+
+@router.put("/change_password/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def change_password(
+    user: user_dependency,
+    db: db_dependency,
+    change_password_request: ChangePasswordRequest,
+    user_id: int = Path(gt=0),
+):
+    if user is None:
+        raise HTTPException(status_code=401, detail="Authenication Failed")
+    user_model = db.query(Users).filter(Users.id == user_id).first()
+
+    if user_model is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    user_model.hashed_password = change_password_request.hashed_password
+    db.add(user_model)
+    db.commit()
