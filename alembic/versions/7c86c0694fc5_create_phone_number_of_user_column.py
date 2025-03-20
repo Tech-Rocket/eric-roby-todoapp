2f2008575ac8828ab45a802fa6ceb0bb2474e703
Ebee02 <beelawrence8@gmail.com>
2025-03-20 17:11:27 +0000

    Added phone number column to the user table using alembic

---
+++ alembic/versions/7c86c0694fc5_create_phone_number_of_user_column.py
@@ -0,0 +1,29 @@
+"""Create phone number of user column
+
+Revision ID: 7c86c0694fc5
+Revises:
+Create Date: 2025-03-20 17:01:08.829516
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "7c86c0694fc5"
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.add_column("users", sa.Column("phone_number", sa.String(), nullable=True))
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
